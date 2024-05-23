26dbae46a107cbeb29908b1f378c78087c6ae00e
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-05-23 17:11:38 +0700

    feat: init UI

---
+++ src/Tubes3_sangku/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tubes3_sangku
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
